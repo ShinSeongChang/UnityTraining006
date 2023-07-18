43f4336516a2697f1e25325e5eb8940811bc2ac0
DESKTOP-51TGSF5\KGA <dlwnsdud0612@gmail.com>
2023-07-18 17:51:58 +0900

    Feat : Add Class Manual

---
+++ Training006 - PathFind/Assets/Scripts/MapControl/TileMapController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapController : MonoBehaviour
+{
+    protected string tileMapObjName = default;
+
+    protected MapBoard mapController = default;
+    protected Tilemap tilemap = default;
+    protected List<GameObject> allTileObjs = default;
+
+    //! Awake 타임에 초기화 할 내용을 상속받은 클래스별로 재정의한다.
+
+    public virtual void InitAwake(MapBoard mapController_)
+    {
+        mapController = mapController_;
+        tilemap = gameObject.FindChildComponent<Tilemap>(tileMapObjName);
+
+        // 직사각형 형태로 초기화 된 타일을 캐싱해서 가지고 있는다.
+        allTileObjs = tilemap.gameObject.GetChildrenObjs();
+
+        if(allTileObjs.IsValid())
+        {
+            allTileObjs.Sort(GFunc.CompareTileObjToLocalPos2D);
+        }
+        else
+        {
+            allTileObjs = new List<GameObject>();            
+        }
+    }
+
+    
+}
